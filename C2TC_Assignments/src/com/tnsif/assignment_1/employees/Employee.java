af3f0b1a0666d08673e6e887967ed0ec4612fbdf
HP <HP@User>
2025-10-02 19:24:36 +0530

    commit

---
+++ C2TC_Assignments/src/com/tnsif/assignment_1/employees/Employee.java
@@ -0,0 +1,39 @@
+package com.tnsif.assignment_1.employees;
+
+public class Employee {
+    private String name;
+    private int employeeId;
+    private double salary;
+
+    // Constructor
+    public Employee(String name, int employeeId, double salary) {
+        this.name = name;
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    protected void setName(String name) {   // protected for subclasses
+        this.name = name;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
