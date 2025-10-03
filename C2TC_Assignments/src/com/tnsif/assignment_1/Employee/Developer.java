a7fc1b52ac16d558329cc0885edc43416c68def5
HP <HP@User>
2025-10-03 09:31:44 +0530

    commit

---
+++ C2TC_Assignments/src/com/tnsif/assignment_1/Employee/Developer.java
@@ -0,0 +1,10 @@
+package com.tnsif.assignment_1.Employee;
+
+public class Developer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2TC_Assignments/src/com/tnsif/assignment_1/Employee/Employee.java
@@ -0,0 +1,20 @@
+package com.tnsif.assignment_1.Employee;
+
+import com.tnsif.assignment_1.employees.Employee;
+
+public class Developer extends Employee {
+    private String programmingLanguage;
+
+    public Developer(String name, int employeeId, double salary, String programmingLanguage) {
+        super(name, employeeId, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    public String getProgrammingLanguage() {
+        return programmingLanguage;
+    }
+
+    public void setProgrammingLanguage(String programmingLanguage) {
+        this.programmingLanguage = programmingLanguage;
+    }
+}
