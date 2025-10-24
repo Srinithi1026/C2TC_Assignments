a72eb35c342e70b577b27146910514ac0799083c
HP <HP@User>
2025-10-24 20:48:07 +0530

    commit

---
+++ C2TC_Assignments/src/com/tnsif/assignment_3/Main.java
@@ -0,0 +1,19 @@
+package com.tnsif.assignment_3;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create accounts
+        SavingsAccount savings = new SavingsAccount("Sathish", 5000);
+        CheckingAccount checking = new CheckingAccount("Kumar", 3000);
+
+        // Create a Transaction object
+        Transaction t = new Transaction();
+
+        // Perform transactions
+        t.performTransaction(savings, "deposit", 1000);
+        t.performTransaction(checking, "withdraw", 500);
+
+        // Display total accounts
+        System.out.println("Total Bank Accounts: " + bank.getTotalAccounts());
+    }
+}
